408fdc21c6702a6a3ae5f6a09f3211c83a3f58ec
Eric Holscher <eric@ericholscher.com>
2012-08-18 12:54:32 -0700

    Refactor redis stuff into utils.

--- src/hydra/api.py
+++ src/hydra/api.py
@@ -1,6 +1,5 @@
 import redis
 
-from django.conf import settings
 from django.conf.urls import url
 
 from tastypie.resources import Resource
@@ -9,49 +8,8 @@ from tastypie.authorization import Authorization
 from tastypie.authentication import Authentication
 from tastypie.exceptions import NotFound
 
-r = redis.StrictRedis.from_url(settings.REDIS_URL)
-
-
-def make_slug(val, version="latest"):
-    return "hydra:v1:redirects:%s:%s" % (version, val)
-
-
-def remove_slug(val, version="latest"):
-    return val.replace(make_slug('', version=version), "")
-
-
-def save(slug, url, version="latest"):
-    redis_slug = make_slug(slug, version)
-    return r.zincrby(redis_slug, url, 1)
-    #r.set(redis_slug, urls)
-
-
-def get_range(pk, withscores=True):
-    return r.zrange(make_slug(pk), 0, -1, withscores=withscores)
-
-
-def get_keys(key):
-    return r.keys(make_slug('%s' % key))
-
-
-def delete(slug):
-    return r.delete(make_slug(slug))
-
-
-def get_urls(slug, count=None):
-    ret_val = []
-    # Could have defaulted to -1 but that is a less obvious API
-    if count is None:
-        count = -1
-    values = get_range(slug)
-    for value in values:
-        redirect_url, score = value
-        ret_val.append({
-            'score': score,
-            'url': redirect_url
-        })
-    return ret_val
-
+from hydra.utils import (r, make_slug, remove_slug, save,
+                         get_range, get_keys, delete, get_urls)
 
 class RedisObject(object):
 
@@ -124,6 +82,9 @@ class HydraResource(Resource):
     def obj_get_list(self, request=None, **kwargs):
         ret_val = []
         filter_slug = request.GET.get('slug', None)
+        if not filter_slug:
+            #Support ?q= format also
+            filter_slug = request.GET.get('q', None)
         if filter_slug:
             keys = get_keys('%s*' % filter_slug)
         else:
--- src/hydra/utils.py
+++ src/hydra/utils.py
@@ -1,8 +1,52 @@
+import redis
 import operator
+
+from django.conf import settings
 
 from sphinx.ext import intersphinx
 
-from hydra.api import r, save, make_slug, get_keys
+r = redis.StrictRedis.from_url(settings.REDIS_URL)
+
+def make_slug(val, version="latest"):
+    return "hydra:v1:redirects:%s:%s" % (version, val)
+
+
+def remove_slug(val, version="latest"):
+    return val.replace(make_slug('', version=version), "")
+
+
+def save(slug, url, version="latest"):
+    redis_slug = make_slug(slug, version)
+    return r.zincrby(redis_slug, url, 1)
+    #r.set(redis_slug, urls)
+
+
+def get_range(pk, withscores=True):
+    return r.zrange(make_slug(pk), 0, -1, withscores=withscores)
+
+
+def get_keys(key):
+    return r.keys(make_slug('%s' % key))
+
+
+def delete(slug):
+    return r.delete(make_slug(slug))
+
+
+def get_urls(slug, count=None):
+    ret_val = []
+    # Could have defaulted to -1 but that is a less obvious API
+    if count is None:
+        count = -1
+    values = get_range(slug)
+    for value in values:
+        redirect_url, score = value
+        ret_val.append({
+            'score': score,
+            'url': redirect_url
+        })
+    return ret_val
+
 
 def safe_save(slug, url):
     if not get_keys(slug):
