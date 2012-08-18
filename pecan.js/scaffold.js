7fbfcac7f1862a266ba04e827269edd5c6fa0219
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-08-17 21:49:54 -0700

    yes yes okay this is something that i enjoy alright

--- pecan.js/scaffold.js
+++ pecan.js/scaffold.js
@@ -4,7 +4,7 @@ var plate = require('plate')
   , templates = require('./templates')
 
 function scaffold(ready) {
-  new plate.Template(templates.initial).render({}, function(err, html) {
+  new plate.Template(templates['initial.html']).render({}, function(err, html) {
     if(err) {
       return ready(err)
     }
