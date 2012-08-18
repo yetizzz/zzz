d9b3f99514cc66ebaf1295032a96f6ee24f31d77
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-08-18 00:45:50 -0700

    there are nice pinball machines here, amirite wraithan, ericholscher

--- pecan.js/views/list.js
+++ pecan.js/views/list.js
@@ -9,9 +9,39 @@ function list(site, resource) {
     var schema = schemas[resource]
 
     request.get(schema.list_endpoint, {}, {}, function(err, items) {
-      site.render('list.html', {results: items}, function(err, el) {
+      items.objects = items.objects.map(getName.bind(schema))
+
+      site.render('list.html', {resource: resource, results: items}, function(err, el) {
 
       })
     })    
   }
 }
+
+function getName(obj, idx, all) {
+  for(var i = 0, len = names.length; i < len; ++i) {
+    if(obj[names[i]]) {
+      obj.__name__ = obj[names[i]]
+      return obj
+    }
+  }
+  
+  for(var key in this) {
+    if(this[key].type === 'string') {
+      obj.__name__ = obj[this[key]]
+      return obj
+    }
+  }
+
+  obj.__name__ = resource_uri
+
+  return obj
+}
+
+var names = [
+    'name'
+  , 'display_name'
+  , 'human_name'
+  , 'title'
+  , 'slug'
+]
