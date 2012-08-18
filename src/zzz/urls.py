abdb17bf36eb9858185cb5e70d4241c193acd49c
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-08-17 21:32:06 -0700

    add admin page

--- src/zzz/urls.py
+++ src/zzz/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, {'template': 'base.html'}, name='home'),
-    # Examples:
-    # url(r'^$', 'src.views.home', name='home'),
-     url(r'^api/', include('hydra.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^_admin/', direct_to_template, {'template': 'admin.html'}, name='admin'),
+    url(r'^api/', include('hydra.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT}),
+    )
+
