b74c2b4173652d2535a5f801e224366395f487f9
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-08-17 19:20:07 -0700

    add JS router

---
+++ pecan.js/routes.js
@@ -0,0 +1,20 @@
+module.exports = route
+
+var views = require('./views')
+
+function route(path) { 
+  var match
+
+  for(var i = 0, len = route.routes.length; i < route.routes.length; ++i) {
+    if(match = route.routes[i][0].exec(path)) {
+      return function() { route.routes[i][1].apply(null, [].slice.call(arguments).concat(match.slice(1))) } 
+    } 
+  }
+}
+
+route.routes = [
+    [/^/?$/, views.root]
+  , [/^(.*?)/, views.list]
+  , [/^(.*?)\/_\/new/, views.create]
+  , [/^(.*?)\/_\/(.*?)/, views.edit]
+]
