3ec4b677afada86c97832854af8cb470022e0e36
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-08-17 19:34:05 -0700

    add views

---
+++ pecan.js/views.js
@@ -0,0 +1,8 @@
+var views = {
+    root: require('./views/root')
+  , list: require('./views/list')
+  , create: require('./views/create')
+  , edit: require('./views/edit')
+}
+
+module.exports = views
---
+++ pecan.js/views/edit.js
@@ -0,0 +1,5 @@
+module.exports = edit
+
+function edit() {
+
+}
