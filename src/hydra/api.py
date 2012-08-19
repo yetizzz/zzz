011535f1e098b1133617ff8ff8962ff76d094b5f
Eric Holscher <eric@ericholscher.com>
2012-08-19 13:38:39 -0700

    Move base_resource to unclutter api.

--- src/hydra/api.py
+++ src/hydra/api.py
@@ -17,8 +17,8 @@ from django.conf import settings
 
 from tastypie.resources import Resource
 from tastypie import fields
-from tastypie.authorization import Authorization
-from tastypie.authentication import Authentication
+from tastypie.authorization import Authorization, DjangoAuthorization
+from tastypie.authentication import Authentication, BasicAuthentication
 from tastypie.exceptions import NotFound, ImmediateHttpResponse
 from tastypie.http import HttpConflict
 
@@ -88,56 +88,6 @@ class RedisProject(object):
 
     def get_whitelist(self):
         return r.smembers("%s:whitelist" % self.redis_slug)
-
-class BaseResource(Resource):
-    """
-    A base resource for all Redis-based Tastypie APIs.
-
-    It expects you to be using the Redis Model interface, which mostly just
-    boils down to save, delete, and exists.
-    """
-    def obj_update(self, bundle, request=None, **kwargs):
-        if bundle.data.get('resource_uri'):
-            del bundle.data['resource_uri']
-        return self.obj_create(bundle, request, delete=True, **kwargs)
-
-    def obj_create(self, bundle, request=None, **kwargs):
-        bundle.obj = self._meta.object_class(**bundle.data)
-        bundle = self.full_hydrate(bundle)
-        if kwargs.get('delete', None):
-            bundle.obj.delete()
-        bundle.obj.save()
-        return bundle
-
-    def obj_get(self, request=None, **kwargs):
-        proj = self._meta.object_class(**kwargs)
-        if not proj.exists():
-            raise NotFound("No object matching this pk")
-        return proj
-
-    def obj_delete(self, request=None, **kwargs):
-        obj = self._meta.object_class(**kwargs)
-        obj.delete()
-
-    def _find_slug(self):
-        if self.fields.get('slug'):
-            return 'slug'
-        if self.fields.get('name'):
-            return 'name'
-
-    def get_resource_uri(self, bundle_or_obj):
-        resource = self._meta.resource_name
-        slug_field = self._find_slug()
-        try:
-            if getattr(bundle_or_obj, 'obj'):
-                return "/_api/v1/%s/%s/" % (resource,
-                                            getattr(bundle_or_obj.obj,
-                                                    slug_field))
-            return "/_api/v1/%s/%s/" % (resource,
-                                        getattr(bundle_or_obj,
-                                                slug_field))
-        except:
-            return ""
 
 class ProjectResource(Resource):
     """
