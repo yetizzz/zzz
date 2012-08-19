3ead24a3959a9cc87f4fff8c95d2ece285bb3fa7
Eric Holscher <eric@ericholscher.com>
2012-08-18 21:37:51 -0700

    Update views.

--- src/hydra/views.py
+++ src/hydra/views.py
@@ -1,10 +1,8 @@
 from django.core.urlresolvers import reverse
-from django.views.generic import RedirectView, FormView, TemplateView
-
+from django.views.generic import RedirectView, TemplateView
 from analytics.models import Visit
 
 from .api import RedisProject, RedisRedirect
-from .forms import SlugForm
 
 
 class SlugLookupRedirectView(RedirectView):
@@ -26,33 +24,30 @@ class SlugLookupRedirectView(RedirectView):
         if not redirect_url:
             Visit.objects.create(key=slug,
                                  retval=', '.join([url['url'] for url in urls]))
-            return reverse('slug-details', kwargs={'slug': slug,
-                                                   'project': project})
+            return reverse('slug-details', kwargs={'slug': slug, 'project': project})
 
         Visit.objects.create(key=slug, retval=redirect_url)
         return redirect_url
 
 
-class SlugDetailView(FormView):
-    form_class = SlugForm
+class SlugDetailView(TemplateView):
+    extra_context = None
 
     def get_context_data(self, *args, **kwargs):
         slug = kwargs.get('slug', '')
         project = kwargs.get('project', '')
         context = super(SlugDetailView, self).get_context_data(*args, **kwargs)
+        context.update(kwargs)
         proj_obj = RedisRedirect(slug=slug, project=project)
         context['urls'] = proj_obj.get_urls()
-        context['slug'] = self.kwargs['slug']
         return context
 
-    def get_success_url(self, *args, **kwargs):
-        return self.request.POST['url']
 
-
-class HomeView(TemplateView):
+class ProjectView(TemplateView):
     extra_context = None
 
     def get_context_data(self, *args, **kwargs):
         context = super(HomeView, self).get_context_data(*args, **kwargs)
         context['projects'] = RedisProject.all_projects()
+        context.update(kwargs)
         return context
